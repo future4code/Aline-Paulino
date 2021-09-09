6f6e1fda76835f3e152343fd4b50c7d7576588e8
AlinePaulinoMartins <alinepama15@gmail.com>
2021-09-08 22:22:19 -0300

    alterei algumas informaçoes do exercicio

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,31 @@
+console.log ("olá mundo!")
+let nome
+let idade
+console.log(typeof nome) // não foi definido um valor para a variavel
+console.log(typeof idade) //não foi definido um valor para a variavel
+let nomeUsuario = prompt("Digite o nome do usuário")
+console.log(nomeUsuario)
+let idadeUsuario = prompt("Digite sua idade")
+console.log(idadeUsuario)
+console.log(typeof nomeUsuario)
+console.log(typeof idadeUsuario)
+console.log("Olá", nomeUsuario, ", você tem", idadeUsuario, "anos")
+let gostaBatata = prompt("voce gosta de batata?")
+console.log("voce gosta de batata?")
+console.log(gostaBatata)
+let gostaTomate = prompt("voce gosta de tomate?")
+console.log("voce gosta de tomate?")
+console.log(gostaTomate)
+let corOlhos = prompt("seus olhos são verdes?")
+console.log("seus olhos são verdes?")
+console.log(corOlhos)
+let a = 10
+let b = 25
+let c = b
+b = a
+a = c
+console.log(a,b)
+
+
+
+
