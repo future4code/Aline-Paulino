5916bfd3d8c2c080872e6141954c0f8c4ea860c9
AlinePaulinoMartins <alinepama15@gmail.com>
2021-09-16 22:26:08 -0300

    testes de funçoes

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,79 @@
+//EXERCICIO 1 - INTERPRETAÇÃO
+//A
+//A função pega os valores ATRIBUIDOS "2 e 10" e mulplica por 5.
+//Apenas imprime no console, Não retorna.
+
+// 2
+//A
+
+// Apos o usuario digitar a frase, converte tudo para caixa baixa
+//e pesquisa se na frase existe a palavra "cenoura".
+
+//B
+
+// I - True 
+// II - True
+// III - False
+
+//EXERCICIO 1 - ESCRITA
+//A
+
+function meusDados() {console.log("Eu sou a Aline, tenho 21 anos, moro em Minas e INTERPRETAÇÃOsou estudante Labenu.")}
+meusDados()
+
+//b
+
+let nome =String (prompt("Digite seu nome"))
+let idade =Number (prompt("Digite sua idade"))
+let estado =String (prompt("Digite seu estado"))
+let profissão =String (prompt("Digite sua profissão"))
+
+function dadosUsuario() {console.log ("Eu sou", nome, ",tenho", idade, "anos, moro em", estado, ",e sou", profissão, ".")}
+dadosUsuario()= String
+
+//EXERCICIO 2
+//A
+
+function soma(numUm,numDois) {console.log (numUm + numDois)}
+soma(10,10)
+
+//B
+
+function maiorOuIgual(numbUm,numbDois) {console.log (numbUm>=numbDois)}
+maiorOuIgual(10,12)
+maiorOuIgual(1,12)
+maiorOuIgual(10,1)
+
+//C
+
+function par(primNum) { console.log ("este numero é um numero par?", (primNum % 2)===0)}
+par(10)
+par(12)
+par(5)
+par(3)
+
+//D
+
+function fraseUsuario(frase) {
+    let tamanho = (frase.length)
+    let maiusculo = frase.toUpperCase()
+    console.log(tamanho,maiusculo)}
+fraseUsuario("gosto de batatinhas")
+
+
+//EXERCICIO 3
+//A
+
+function contas(nUm,nDois) {
+    console.log("números inseridos", nUm, "e" ,nDois)
+    let subtracao = nUm - nDois
+    let soma = nUm + nDois
+    let mulplicacao = nUm * nDois
+    let divisao = nUm / nDois
+    console.log("subtração =", subtracao)
+    console.log("soma =" ,soma)
+    console.log("divisão =", divisao)
+    console.log("multiplicação =", mulplicacao)}
+    let primeiroNumero =Number (prompt("Digite um numero"))
+    let segundoNumero =Number (prompt("Digite outro numero"))
+    contas(primeiroNumero,segundoNumero)
