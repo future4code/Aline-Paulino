a141cc092a334b38abd5410b4883b3df63c7b7af
Aline <alinepama15@gmail.com>
2021-10-07 22:04:56 -0300

    quinzena 3 revisão

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -70,29 +70,75 @@ function retornaNNumerosPares(n) {
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
+  if (a === b && b === c && a === c) {
+    return 'Equilátero'
+  } else if (a === b && b !== c && a !== c) {
+    return 'Isósceles'
+  } else (a !== b && b !== c && a !== c)
+    return'Escaleno'
+  }
   // return 'Escaleno'
   // return 'Equilátero'
   // return 'Isósceles'
-}
+
 
 // EXERCÍCIO 09
 function comparaDoisNumeros(num1, num2) {
+  let a
+  let b
+  let c
+ if (num1>num2){
+   a = num1}
+   else{ a = num2}
+
+ if (num1 % num2 === 0 || num2 % num1 ===0 ){
+  b = true
+ }
+ else{b = false}
+
+c = Math.abs(num2-num1)
+
+  const objeto = {
+    maiorNumero: a,
+    maiorDivisivelPorMenor: b,
+    diferenca: c
+  }
+  return objeto
+}
   // Formato do objeto a ser retornado:
   // {
   //   maiorNumero: X,
   //   maiorDivisivelPorMenor: Y,
   //   diferenca: Z
   // }
-}
-
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
-
+  let numerosOrganizados = array.sort(function(a, b) {
+    return a - b;
+});
+  let novaArray = []
+  novaArray.push(numerosOrganizados[numerosOrganizados.length-2] ,numerosOrganizados[1])
+  return novaArray
 }
-
 // EXERCÍCIO 11
 function ordenaArray(array) {
+  let auxiliar
+  let controle
+  for(let i = 0; i < array.length; ++i) {
+    controle = true
+    for(let i = 0; i < (array - 1); ++i) {
 
+      if(array[i] > array[i + 1]){
+        auxiliar = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = auxiliar
+        controle = false;
+      }
+    }
+    if(controle){
+      break;
+    }
+  }
 }
 
 // EXERCÍCIO 12
@@ -161,6 +207,4 @@ function ordenaPorData(consultasData) {
 }
 
 // EXERCÍCIO 20
-function calculaSaldo(contas) {
-
-}
+function calculaSaldo(contas) {}
