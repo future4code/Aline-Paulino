135e1d39545b065c0e79c44951f749dab6acb0ca
AlinePaulinoMartins <alinepama15@gmail.com>
2021-11-23 22:30:12 -0300

    tentativa 1

---
+++ rend-condicional-form/labenu-forms/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+class App extends React.Component {
+  state = {
+    etapa: 1,
+  }
+  render () {
+  return (
+    <div>
+      <button>Próxima etapa</button>
+    </div>
+  );
+}}
+
+export default App;
---
+++ rend-condicional-form/labenu-forms/src/components/Etapa2.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div> Boa Noite
+    </div>
+  );
+}
+
+export default App;
